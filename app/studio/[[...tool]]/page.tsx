fd4a3d11bc3fdde0b97c4966ce5cab135a9c9470
Brendan <brendanbundy1@gmail.com>
2025-10-30 21:10:54 -0600

    Remove metadata and viewport exports from page.tsx to streamline StudioPage component

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -11,8 +11,6 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
 
-export { metadata, viewport } from "next-sanity/studio";
-
 export const dynamic = "force-static";
 
 export default function StudioPage() {
